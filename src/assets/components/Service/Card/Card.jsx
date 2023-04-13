662abebd1eee0457c2d20cc9441aa27daeda4f14
Pluhin <Pluhin@itpc.ru>
2023-04-13 13:04:36 +0500

    Рендер списка

--- src/assets/components/Service/Card/Card.jsx
+++ src/assets/components/Service/Card/Card.jsx
@@ -1,16 +1,20 @@
-import s from './Card.module.css'
+import s from './Card.module.scss'
+import png from '../../../img/example.png'
 
 export const Card = ({ title, list }) => {
   return (
     <div className={s.card}>
       <p className={s.title}>{title}</p>
-      <ul className={s.content}>
-        {list.map((item, i) => (
-          <li key={i} className={s.text}>
-            {item}
-          </li>
-        ))}
-      </ul>
+      <div className={s.wrapper}>
+        <img src={png} alt="пример" className={s.image} />
+        <ul className={s.content}>
+          {list.map((item, i) => (
+            <li key={i} className={s.text}>
+              <p className={s.subtext}>{item}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
       <button className={s.btn}>Получить помощь</button>
     </div>
   )
